f58fd22828efaddea1de88a8f1b64501e3cb70f4
Josefisa <j.emanuel.figue@gmail.com>
2025-06-29 23:58:16 -0500

    Otra actualización del dia 32

--- ProyectosIntermedios/Day-32-Email_Dates/main.py
+++ ProyectosIntermedios/Day-32-Email_Dates/main.py
@@ -1,11 +1,24 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 mi_correo = "stalin.criollo.arg@gmail.com"
 clave = "eyjkoembwmqxnrqt"
 
-coneccion = smtplib.SMTP("smtp.gmail.com")
-coneccion.starttls()
-coneccion.login(user=mi_correo, password=clave)
-texto = "¡Viva la libertad carajo!".encode('utf-8')
-coneccion.sendmail(from_addr=mi_correo, to_addrs="stalin.criollo.arg@outlook.com", msg=texto)
-coneccion.close
+with smtplib.SMTP("smtp.gmail.com") as coneccion:
+    coneccion.starttls()
+    coneccion.login(user=mi_correo, password=clave)
+    
+    mensaje = MIMEMultipart()
+    mensaje['from'] = mi_correo
+    mensaje['to'] = 'stalin.criollo.arg@outlook.com'
+    mensaje['subject'] = "¡Viva la libertad carajo"
+    
+    with open('ProyectosIntermedios/Day-32-Email_Dates/body.txt', 'r', encoding='utf-8') as file:
+        cuerpo = file.read()
+        
+        
+    mensaje.attach(MIMEText(cuerpo, 'plain', 'utf-8'))
+    coneccion.sendmail(from_addr=mi_correo, 
+                       to_addrs="stalin.criollo.arg@outlook.com", 
+                       msg=mensaje.as_string())
