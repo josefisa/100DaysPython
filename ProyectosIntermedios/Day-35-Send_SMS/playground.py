26f524ebc6577e2d2b369bf592796444bbdb731a
Josefisa <j.emanuel.figue@gmail.com>
2025-07-08 23:46:09 -0500

    A little bit added.

--- ProyectosIntermedios/Day-35-Send_SMS/playground.py
+++ ProyectosIntermedios/Day-35-Send_SMS/playground.py
@@ -1,7 +1,22 @@
+import requests
+import json
 
 MY_API = "c3cd3aefc40b0d701348417944352f33"
 LNG =   4.659 
 LAT = -74.090
 
-#For now I'm a little bit stucks as the API takes some time to activate. :)
-#So now let's code in C, I guess and lets solve Dynamical Systems problems. .)
+parameters = {
+    "lat": LAT,
+    "lon": LNG,
+    "appid": MY_API,
+}
+
+try:
+    response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    #print(json.dumps(data, indent= 4))
+except requests.exceptions.RequestException as exe:
+    print(f"Error perfoming request: {exe}")
+
+
