eb2ea65e4ac0a3ea3788d44b9469e0a402962c71
Josefisa <j.emanuel.figue@gmail.com>
2025-07-09 20:01:51 -0500

    Day 35 finished

--- ProyectosIntermedios/Day-35-Send_SMS/playground.py
+++ ProyectosIntermedios/Day-35-Send_SMS/playground.py
@@ -1,9 +1,14 @@
 import requests
 import json
+import os
+from twilio.rest import Client
 
-MY_API = "c3cd3aefc40b0d701348417944352f33"
+MY_API = os.environ.get("OWM_API_KEY")
 LNG =   4.659 
 LAT = -74.090
+
+account_sid = "_MY_TWILIO_ACCOUNT_SID_"
+auth_token = os.environ.get("TWILIO_AUT_TOKEN")
 
 parameters = {
     "lat": LAT,
@@ -14,9 +19,22 @@ parameters = {
 try:
     response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
     response.raise_for_status()
-    data = response.json()
+    data = response.json()["list"]
     #print(json.dumps(data, indent= 4))
 except requests.exceptions.RequestException as exe:
     print(f"Error perfoming request: {exe}")
+    
+    
+weather_id = [item["weather"]["id"] for item in data]
+description = [item["weahter"][0]["description"] for item in data]
+timestamp = [item["dt_txt"] for item in data]
 
 
+client = Client(account_sid, auth_token)
+message = client.messages.create(
+    body=f"Today {timestamp[0]} is goin to be {description[0]}",
+    from_="YOUR TWILIO VIRTUAL NUMBER",
+    to="YOUR TWILIO VERIFIED REAL NUMBER"
+)
+
+print(message.status)
